876b7ec77b32a30ad66214c7b5cf44489d3b627c
BettingIsCool <contact@bettingiscool.com>
2025-04-08 16:16:24 +0200

    Remove redundant `.to_dict('records')` from query result
    The `.to_dict('records')` conversion was unnecessary as the query result is already sufficient for processing. Simplifying this ensures cleaner and more efficient code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,9 @@ selected_sport = st.selectbox(label='Sport', options=SPORTS.keys(), help='You ca
 selected_from_date = st.date_input(label='Start date', value=datetime.date(year=2021, month=1, day=1), min_value=datetime.date(year=2021, month=1, day=1), help='When should your data start? You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
 selected_to_date = st.date_input(label='End date', value='today', min_value=datetime.date(year=2021, month=1, day=1), help='When should your data end? You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
 
-leagues = db.get_unique_leagues(sport=selected_sport, date_from=selected_from_date, date_to=selected_to_date)['league_name']
+leagues = db.get_unique_leagues(sport=selected_sport, date_from=selected_from_date, date_to=selected_to_date)['league_id']
 
-sorted_leagues = list(leagues.values())
+sorted_leagues = list(leagues.keys())
 sorted_leagues.sort()
 st.write(sorted_leagues)
 
