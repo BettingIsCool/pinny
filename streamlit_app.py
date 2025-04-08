f93974d701940b2a18cd0af22e9be754df91a9d6
BettingIsCool <contact@bettingiscool.com>
2025-04-08 16:11:33 +0200

    Remove redundant `.to_dict('records')` from query result
    The `.to_dict('records')` conversion was unnecessary as the query result is already sufficient for processing. Simplifying this ensures cleaner and more efficient code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ selected_to_date = st.date_input(label='End date', value='today', min_value=date
 leagues = db.get_unique_leagues(sport=selected_sport, date_from=selected_from_date, date_to=selected_to_date)['league_name']
 
 sorted_leagues = list(leagues.keys())
-print(type(sorted_leagues))
+st.write(type(sorted_leagues))
 
 selected_leagues = st.multiselect(label='Leagues', options=sorted_leagues, format_func=lambda x: leagues.get(x), placeholder='Start typing...', help='Please select the leagues you need the data for.')
 
