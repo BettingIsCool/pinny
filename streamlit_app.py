5832f7d0a271439d402205f7404b6e3f2b1a2bae
BettingIsCool <contact@bettingiscool.com>
2025-04-08 14:35:50 +0200

    Add database integration to fetch and display leagues
    Introduce `db_pinny.py` for querying leagues from the database using sport IDs. Update `streamlit_app.py` to display fetched leagues with Streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.set_page_config(page_title="PinnacleData by BettingIsCool", page_icon="💾", layout="wide", initial_sidebar_state="expanded")
-st.markdown('## **PinnacleData by BettingIsCool. Structured betting data.**')
+st.title('PinnacleData by BettingIsCool.')
+st.subheader('Structured betting data')
 
 import db_pinny as db
 from config import SPORTS
