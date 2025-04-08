e255f0aaf1117be28374f82d1f331a954d701af7
BettingIsCool <contact@bettingiscool.com>
2025-04-08 15:19:16 +0200

    Refactor league query and add type selection in UI.
    Updated `get_leagues` to `get_user_unique_leagues` with new parameters and refined SQL query to ensure unique league filtering by date. Added a `type` selection dropdown in the Streamlit app for granular filtering, and updated table constants to replace `TABLE_USERS` with `TABLE_OPENING` and `TABLE_CLOSING`.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,13 @@ import db_pinny as db
 
 from config import SPORTS
 
+selected_type = st.selectbox(label='Type', options=['Opening', 'Closing', 'Granular'], help='What type of data do you need? Opening...opening odds, Closing...closing odds, Granular...all odds (the complete odds history for each match updated roughly every 10 seconds).')
 selected_sport = st.selectbox(label='Sport', options=SPORTS.keys(), help='You can request only one sport at a time. If you need data for more sports please submit multiple requests.')
 selected_from_date = st.date_input(label='Start date', value=datetime.date(year=2021, month=1, day=1), min_value=datetime.date(year=2021, month=1, day=1), help='When should your data start? You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
 selected_to_date = st.date_input(label='End date', value='today', min_value=datetime.date(year=2021, month=1, day=1), help='When should your data end? You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
+
+
+
 
 #leagues = db.get_leagues(sport_id=19)
 #st.sidebar(leagues)
