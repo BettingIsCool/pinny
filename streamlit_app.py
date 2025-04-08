2c8e6687eda0a48f6ca1de0a1ca8f78bd019d5f8
BettingIsCool <contact@bettingiscool.com>
2025-04-08 16:19:27 +0200

    Remove redundant `.to_dict('records')` from query result
    The `.to_dict('records')` conversion was unnecessary as the query result is already sufficient for processing. Simplifying this ensures cleaner and more efficient code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,14 @@ selected_sport = st.selectbox(label='Sport', options=SPORTS.keys(), help='You ca
 selected_from_date = st.date_input(label='Start date', value=datetime.date(year=2021, month=1, day=1), min_value=datetime.date(year=2021, month=1, day=1), help='When should your data start? You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
 selected_to_date = st.date_input(label='End date', value='today', min_value=datetime.date(year=2021, month=1, day=1), help='When should your data end? You can either use the calendar or manually enter the date, i.e. 2024/08/19.')
 
-leagues = db.get_unique_leagues(sport=selected_sport, date_from=selected_from_date, date_to=selected_to_date)['league_name']
+leagues = db.get_unique_leagues(sport=selected_sport, date_from=selected_from_date, date_to=selected_to_date)
 
-sorted_leagues = list(leagues.keys())
-sorted_leagues.sort()
+st.write(leagues)
+#sorted_leagues = list(leagues.keys())
+#sorted_leagues.sort()
 
-selected_leagues = st.multiselect(label='Leagues', options=sorted_leagues, format_func=lambda x: leagues.get(x), placeholder='Start typing...', help='Please select the leagues you need the data for.')
-st.write(selected_leagues)
+#selected_leagues = st.multiselect(label='Leagues', options=sorted_leagues, format_func=lambda x: leagues.get(x), placeholder='Start typing...', help='Please select the leagues you need the data for.')
+#st.write(selected_leagues)
 
 #selected_leagues = st.selectbox("Leagues", options=sorted(leagues))
 
