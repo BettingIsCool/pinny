300a3b6ba17eec3d0af6131792a2d605c8d844c0
BettingIsCool <contact@bettingiscool.com>
2025-04-09 00:28:00 +0200

    Refactor period selection logic in Streamlit app.
    Adjusted the periods dropdown to dynamically filter options based on the selected sport. This improves user experience by showing only relevant period choices for the chosen sport.

--- db_pinny.py
+++ db_pinny.py
@@ -13,6 +13,6 @@ def get_unique_leagues(sport: str, date_from: datetime, date_to: datetime):
 
 def get_closing_event_ids(date_from: datetime, date_to: datetime, league_ids: list, markets: list, periods: list):
 
-    return conn.query(f"SELECT event_id FROM {TABLE_CLOSING} WHERE sport_name = '{sport}' AND starts >= '{date_from.strftime('%Y-%m-%d %H:%M:%S')}' AND starts <= '{date_to.strftime('%Y-%m-%d %H:%M:%S')}'")
+    return conn.query(f"SELECT COUNT(event_id) FROM {TABLE_CLOSING} WHERE starts >= '{date_from.strftime('%Y-%m-%d %H:%M:%S')}' AND starts <= '{date_to.strftime('%Y-%m-%d %H:%M:%S')}' AND league_id IN {league_ids} AND market IN {markets} AND period IN {periods}")
 
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -17,17 +17,26 @@ selected_to_date = st.date_input(label='End date', value='today', min_value=date
 # Get leagues
 leagues_df = db.get_unique_leagues(sport=selected_sport, date_from=selected_from_date, date_to=selected_to_date)
 leagues = dict(zip(leagues_df.league_id, leagues_df.league_name))
-
 selected_leagues = st.multiselect(label='Leagues', options=sorted(leagues.keys()), format_func=lambda x: leagues.get(x), placeholder='Start typing...', help='Please select the leagues you need the data for.')
 selected_leagues_string = [f"'{s}'" for s in selected_leagues]
 selected_leagues_string = f"({','.join(selected_leagues_string)})"
 
-st.write(selected_leagues_string)
-
+# Process markets
 selected_markets = st.multiselect(label='Markets', options=['moneyline', 'spread', 'totals', 'home_totals', 'away_totals'], help='Please select the markets you need the data for.')
+selected_markets_string = [f"'{s}'" for s in selected_markets]
+selected_markets_string = f"({','.join(selected_markets_string)})"
 
+# Process periods
 period_options = {k[1]: v for k, v in PERIODS.items() if k[0] == SPORTS[selected_sport]}
 selected_periods = st.multiselect(label='Periods', options=period_options.keys(), format_func=lambda x: period_options.get(x), help='Please select the periods you need the data for.')
+selected_periods_string = [f"'{s}'" for s in selected_periods]
+selected_periods_string = f"({','.join(selected_periods_string)})"
+
+if selected_type == 'Closing':
+
+    rowcount = db.get_closing_event_ids(date_from=selected_from_date, date_to=selected_to_date, league_ids=selected_leagues, markets=selected_markets, periods=selected_periods)
+    st.write(rowcount)
+
 
 
 
