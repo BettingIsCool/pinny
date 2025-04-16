083861198dcb532b8a81347a00fd92e0650bdda3
BettingIsCool <contact@bettingiscool.com>
2025-04-17 00:03:23 +0200

    Refactor large queries to handle event IDs in chunks
    Introduced a generator function to split large event ID lists into smaller chunks for processing. Updated the query logic in `streamlit_app.py` to utilize this chunking, improving robustness and preventing performance bottlenecks with large datasets.

--- streamlit_app.py
+++ streamlit_app.py
@@ -144,7 +144,8 @@ if selected_leagues != '()':
                     chunk = [f"{s}" for s in chunk]
                     chunk = f"({','.join(chunk)})"
                     rowcount += db.get_granular_rowcount(event_ids=chunk, markets=selected_markets, periods=selected_periods)[0]['COUNT(id)']
-                    placeholder2.write(f"Processed {n} events so far...")
+                    placeholder2.empty()
+                    placeholder2.write(f":green[Processed {n} events so far]")
 
                 #event_ids = [f"{s}" for s in event_ids]
                 #event_ids = f"({','.join(event_ids)})"
